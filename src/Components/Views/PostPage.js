becbba3090d0beec3b80797417ae801be539efc7
Nano Productions <razzakammar.edu@gmail.com>
2019-10-28 22:32:01 -0400

    Moved Files

--- src/Components/Views/PostPage.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-class postPage extends React.Component {
-  render() {
-    const id = this.props.match.params.id;
-    return <h1>PostPage = {id}</h1>;
-  }
-}
-
-export default postPage;
