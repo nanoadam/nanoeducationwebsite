b6820259933601f253152222c57e1bca0550ebe6
Nano Productions <razzakammar.edu@gmail.com>
2019-10-28 23:23:17 -0400

    Added Files

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <Header />
+    <div id="nav">
+      <router-link to="/">Home</router-link>|
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view />
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header";
+export default {
+  components: {
+    Header
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+  <header>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary"></nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header"
+};
+</script>
+
+<style>
+</style>
